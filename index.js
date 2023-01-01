e8652fc11954deb2ef18879c3fed5f38091c039b
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-01-02 00:22:48 +0600

    project setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 6000;
