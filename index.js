3e3c2028196fd06f1f42eabd49cece1a419ff37e
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-01-02 00:20:05 +0600

    start project

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI)
+
+    .then(() => {
+        app.get("/", (req, res) => {
+            res.send("inventory server is running");
+        })
+
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`)
+        })
+    })
+    .catch(err => console.log(err)) 
