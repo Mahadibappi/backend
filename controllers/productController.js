ba44ee34e6c322739650769f4a2825e2df100592
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-01-14 22:40:12 +0600

    product controller created

---
+++ controllers/productController.js
@@ -0,0 +1,5 @@
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
+const { fileSizeFormatter } = require("../utils/fileUpload");
+const cloudinary = require("../utils/cloudinary");
+
