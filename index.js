f3ead413a324224234327157911616c31114cdca
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-02 17:53:27 +0600

    update continue

--- index.js
+++ index.js
@@ -26,10 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://inventory-frontend-umber.vercel.app",
-    ],
+    origin: "*",
     credentials: true,
   })
 );
