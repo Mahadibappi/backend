52139c67a2df3125ee15f093e99e45bdb53c2d99
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-02 12:52:24 +0600

    domain added

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://inventory-frontend-umber.vercel.app",
+    ],
     credentials: true,
   })
 );
