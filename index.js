ef4f70e55f3b72ae6da19dd31ba375f8b372a8f6
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-03 00:09:20 +0600

    vercel config

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
