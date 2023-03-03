bce8bed92fa30345ccd77e8b22abf38586b9e803
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-03 09:13:18 +0600

    set header

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const { createProduct,
-    getProducts,
-    getProduct,
-    deleteProduct,
-    updateProduct } = require('../controllers/productController');
+const express = require("express");
+const {
+  createProduct,
+  getProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/productController");
 const router = express.Router();
 
 const protect = require("../middleWare/authMiddleware");
 
-router.post("/", protect, createProduct);
-router.patch("/:id", protect, updateProduct)
+router.post(
+  "/",
+  (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  },
+  protect,
+  createProduct
+);
+router.patch("/:id", protect, updateProduct);
 router.get("/", protect, getProducts);
 router.get("/:id", protect, getProduct);
 router.delete("/:id", protect, deleteProduct);
