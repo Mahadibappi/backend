ae09bc032cf887d001d1b9522107763015d15853
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-04 00:41:15 +0600

    commit

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -10,17 +10,6 @@ const router = express.Router();
 
 const protect = require("../middleWare/authMiddleware");
 
-router.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
-});
-
 router.post("/", protect, createProduct);
 router.patch("/:id", protect, updateProduct);
 router.get("/", protect, getProducts);
