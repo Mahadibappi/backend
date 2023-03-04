34ef974f5ad015867844f189033b49ba0982b8fd
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-04 11:36:02 +0600

    commit

--- index.js
+++ index.js
@@ -30,23 +30,6 @@ app.use(
   cors({
     origin: ["http://localhost:3000"],
     credentials: true,
-    headers: [
-      {
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ],
   })
 );
 
