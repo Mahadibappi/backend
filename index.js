6d4b25b7c8f4a08b0275e652060fe5328d5eda3d
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-04 10:07:49 +0600

    commit

--- index.js
+++ index.js
@@ -33,16 +33,16 @@ const corsOrigin = {
 };
 app.use(cors(corsOrigin));
 
-app.use((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
-});
+// app.use((req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Max-Age", "1800");
+//   res.setHeader("Access-Control-Allow-Headers", "content-type");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+//   );
+// });
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
