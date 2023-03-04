2a718d9429986137b0ab39fd77ed9938025c4619
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-04 11:28:02 +0600

    headers again

--- index.js
+++ index.js
@@ -27,31 +27,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(
-  cors(
-    {
-      origin: ["http://localhost:3000"],
-      credentials: true,
-    },
-    {
-      headers: [
-        {
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value:
-                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-            },
-          ],
-        },
-      ],
-    }
-  )
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    headers: [
+      {
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ],
+  })
 );
 
 // app.use((req, res) => {
