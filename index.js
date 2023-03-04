bccfedd6b67c0e348297e61649d92e797bf9d86a
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-04 17:44:07 +0600

    METHODS ADDED

--- index.js
+++ index.js
@@ -29,20 +29,10 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
-
-// app.use((req, res) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Max-Age", "1800");
-//   res.setHeader("Access-Control-Allow-Headers", "content-type");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-//   );
-// });
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
