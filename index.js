4f84037cb98fc17323eacea7ff40c1cda50be32c
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-05 09:12:19 +0600

    middle ware change

--- index.js
+++ index.js
@@ -16,10 +16,15 @@ const router = require("./routes/productRouter");
 const PORT = process.env.PORT || 6000;
 
 //middlewares
-app.options("*", cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Origin",
     "Origin, X-Requested-With, Content-Type, Accept"
